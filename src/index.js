d3b0e9ea8bca90763fe878e3a5ef842eb22ed0f8
Marcel Reis-Soubkovsky <marcelclemente.msc@gmail.com>
2021-10-07 21:58:52 +0200

    canvas pixel paint trial

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@
 // title.innerHTML = "Damn";
 import "./assets/main.css";
 
-const newDiv = document.createElement("h1");
-newDiv.innerHTML = "Test123";
-document.body.append(newDiv);
-
 const canvas = document.getElementById("canvas");
-canvas.style.background = "cyan";
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const ctx = canvas.getContext("2d");
+// canvas.style.background = "cyan";
+
+const imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+const data = imageData.data;
+
+for (const y = 0; y < canvasHeight; ++y) {
+  for (const x = 0; x < canvasWidth; ++x) {
+    const index = (y * canvasWidth + x) * 4;
+    const red = index;
+    const green = index + 1;
+    const blue = index + 2;
+
+    imageData.data[red] = 0;
+    imageData.data[green] = 255;
+    imageData.data[blue] = 0;
+  }
+}
+
+ctx.drawImage(imageData.data, 0, 0);
