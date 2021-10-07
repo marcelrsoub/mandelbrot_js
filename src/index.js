8aef291559970c9a8b900a817255d3966298f207
Marcel Reis <marcelclemente.msc@gmail.com>
2021-10-07 02:15:06 +0200

    Webpack Boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// const title = document.getElementById("title");
+// title.innerHTML = "Damn";
+import "./assets/main.css";
+
+const newDiv = document.createElement("h1");
+newDiv.innerHTML = "Test123";
+document.body.append(newDiv);
+
+const canvas = document.getElementById("canvas");
+canvas.style.background = "cyan";
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+require("@babel/register");
+
+module.exports = {
+  mode: "development",
+  entry: ["webpack/hot/dev-server", "./src/index.js"],
+  output: {
+    filename: "static/js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+  resolve: {
+    extensions: ["", ".js", ".jsx", ".css"],
+    modules: [path.resolve("./src"), path.resolve("./node_modules")],
+  },
+  devServer: {
+    hot: true,
+  },
+  devtool: "source-map",
+};
