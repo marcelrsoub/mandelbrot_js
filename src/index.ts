8413087ec1571d25a91e1ededcb22a3eaffe66b6
Marcel Reis Soubkovsky <marcelclemente.msc@gmail.com>
2021-11-18 19:19:55 +0100

    new mandel Observer Pattern

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,172 @@
 import "./assets/main.css";
 
 import MandelbrotCalculation from "./mandelbrot";
-const points = 500;
-const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-canvas.width = points;
-canvas.height = points;
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
-const ctx = canvas.getContext("2d");
 
-// canvas.style.background = "cyan";
+class Model {
+  mandelbrot: MandelbrotCalculation;
+  observers: any[];
+  mask: number[];
+  points: number;
 
-const imageData = ctx.createImageData(canvasWidth, canvasHeight);
-const data = imageData.data;
+  constructor(points) {
+    this.mandelbrot = new MandelbrotCalculation(points);
+    this.points = points;
+    this.observers = [];
+    this.mask = this.mandelbrot.core();
+  }
 
-const multiplier = 255;
+  update(mouseSubject) {
+    const delta_mandel =
+      (this.mandelbrot.x_high - this.mandelbrot.x_low) / this.points;
+    const dx: number = mouseSubject.x * delta_mandel;
+    const dy = mouseSubject.y * delta_mandel;
+    console.log("mousemove X:", mouseSubject.x);
+    console.log("mousemove Y:", mouseSubject.y);
+    console.log("dx:", dx);
+    console.log("dy:", dy);
+    this.move(dx, dy);
+  }
 
-const Mandelbrot = new MandelbrotCalculation(points);
-Mandelbrot.applyPixel = (i, j, intensity,x_coordinates,y_coordinates) => {
-  const index = (j * points + i) * 4;
-    const red = index;
-    const green = index + 1;
-    const blue = index + 2;
-    const alpha = index + 3;
+  move(dx, dy) {
+    this.mandelbrot.x_low += dx;
+    this.mandelbrot.x_high += dx;
+    this.mandelbrot.y_high += dy;
+    this.mandelbrot.y_low += dy;
+    this.mask = this.mandelbrot.core();
+    this.notifyObservers();
+  }
 
-    // data[red] = (1/Math.tan(y_coordinates/x_coordinates))*multiplier;
-    data[red] = Math.sqrt(x_coordinates**2+y_coordinates**2)*multiplier;
-    data[green] = Math.sqrt((x_coordinates+1)**2+(y_coordinates)**2)*multiplier;
-    data[blue] = (Math.sqrt((x_coordinates)**2+(y_coordinates+1)**2))*multiplier;
-    data[alpha] = intensity*multiplier;
-  // ctx.fillStyle = `rgba(255,255,255,${intensity * 10})`;
-  // ctx.fillRect(i, j, 1, 1)
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  notifyObservers() {
+    for (let o of this.observers) {
+      o.update(this);
+    }
+  }
 }
-Mandelbrot.core();
-ctx.putImageData(imageData, 0, 0);
 
+class CanvasObserver {
+  canvasEl: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  imageData: any;
+  data: any;
+  points: number;
 
+  constructor(canvas: HTMLCanvasElement, points) {
+    this.points = points;
+    canvas.width = points;
+    canvas.height = points;
+    this.canvasEl = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.imageData = this.ctx.createImageData(canvas.width, canvas.height);
 
+    /** data has 4 values for each pixel [R,G,B,A], so length is equal to 4 times the number of pixels */
+    this.data = this.imageData.data;
+    if (this.data.length !== this.points ** 2 * 4) {
+      throw "ERROR:" + this.data.length + "!==" + this.points ** 2 * 4;
+    }
+  }
+
+  update(model) {
+    const data = this.imageData.data;
+    if (this.data.length !== model.mask.length * 4) {
+      throw "ERROR:" + this.data.length + "!==" + model.mask.length * 4;
+    }
+    model.mask.forEach((pixel: number, index: number) => {
+      const index_calc = index * 4;
+      const red = index_calc;
+      const green = index_calc + 1;
+      const blue = index_calc + 2;
+      const alpha = index_calc + 3;
+      data[red] = pixel * 255;
+      data[green] = pixel * 255;
+      data[blue] = pixel * 255;
+      data[alpha] = pixel * 255;
+    });
+    this.ctx.putImageData(this.imageData, 0, 0);
+  }
+
+  paintWhite() {
+    const data = this.imageData.data;
+    Array(this.points ** 2)
+      .fill(0)
+      .map((pixel: number, index: number) => {
+        const index_calc = index * 4;
+        const red = index_calc;
+        const green = index_calc + 1;
+        const blue = index_calc + 2;
+        const alpha = index_calc + 3;
+
+        data[red] = 255;
+        data[green] = 255;
+        data[blue] = 255;
+        data[alpha] = 2555;
+      });
+
+    this.ctx.putImageData(this.imageData, 0, 0);
+    console.log("canvas painted white");
+  }
+}
+
+class MouseSubject {
+  canvas: HTMLCanvasElement;
+  mousedown: boolean;
+  observers: any[];
+  x: number;
+  y: number;
+
+  constructor(canvas) {
+    this.canvas = canvas;
+
+    this.observers = [];
+    this.mousedown = false;
+    canvas.addEventListener("mousedown", () => {
+      this.mousedown = true;
+      console.log("mousedown");
+    });
+    canvas.addEventListener("mouseup", () => {
+      this.mousedown = false;
+      console.log("mouseup");
+    });
+    canvas.addEventListener("mousemove", (e: MouseEvent) => {
+      const screenLimit = this.canvas.width;
+      if (this.mousedown) {
+        this.x = (2 * e.movementX) / screenLimit - 1;
+        this.y = (2 * e.movementY) / screenLimit - 1;
+        console.log(this.x);
+
+        this.notifyObservers();
+      }
+    });
+  }
+
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  notifyObservers() {
+    for (let o of this.observers) {
+      o.update(this);
+    }
+  }
+}
+
+const points = 100;
+const myModel = new Model(points);
+
+const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+const canvasObs = new CanvasObserver(canvas, points);
+// canvasObs.paintWhite();
+const mouseSub = new MouseSubject(canvas);
+mouseSub.addObserver(myModel);
+
+myModel.addObserver(canvasObs);
+myModel.notifyObservers();
+
+// canvas.addEventListener('mousedown', ()=>{console.log('mousedown')})
+// canvas.addEventListener('mousemove', (e) => {
+//   console.log('mousemove X:', e.x)
+//   console.log('mousemove Y:', e.y)
+// })
--- src/mandelbrot.ts
+++ src/mandelbrot.ts
@@ -3,53 +3,66 @@ import { add, Complex, complex, pow, range, sqrt } from "mathjs";
 export default class MandelbrotCalculation {
   points: number;
   limits: number[];
+  canvas: number[];
   max: number;
+  x_low:number;
+x_high:number;
+y_low:number;
+y_high:number;
+
+  
   applyPixel:(i:number,j:number,intensity:number,x_coordinates:number,y_coordinates:number)=>void | undefined
 
   constructor(points) {
     this.points = points;
     this.max = 0;
+    this.x_low = -2.5
+    this.x_high = 1
+    this.y_low = -1.75
+    this.y_high = 1.75
   }
-  core(limits = [-2.5, 1, -1.75, 1.75], iteration_number = 20, boundary = 2) {
+  core( iteration_number = 20, boundary = 2) {
+    const mask = Array(this.points ** 2).fill(0)
+    
     const startTime = performance.now()
-    this.limits = limits;
 
     const x= range(
-      limits[0],
-      limits[1],
-      (limits[1] - limits[0]) / this.points
+      this.x_low,
+      this.x_high,
+      (this.x_high - this.x_low) / this.points
     ).toArray();
     const y = range(
-      limits[2],
-      limits[3],
-      (limits[3] - limits[2]) / this.points
+      this.y_low,
+      this.y_high,
+      (this.y_high - this.y_low) / this.points
     ).toArray();
 
 
     for (let i = 0; i < x.length; i++) {
       for (let j = 0; j < y.length; j++) {
-        const x_pixel = x[i] as number;
-        const y_pixel = y[j] as number;
+        const index = (j * this.points + i);
 
-        let c = complex(x_pixel, y_pixel);
+        const x_pixel_value = x[i] as number;
+        const y_pixel_value = y[j] as number;
+
+        let c = complex(x_pixel_value, y_pixel_value);
         let z = c;
         
         for (let iteration = 0; iteration <= iteration_number; iteration++) {
           z = <Complex>add(pow(z,2), c);
           const boundaryCheck = sqrt(z.im ** 2 + z.re ** 2);
-          if ( boundaryCheck >= boundary) {
+          if (boundaryCheck >= boundary) {
             if (iteration > this.max) {
               this.max = iteration;
             }
-            // PAINTING LAYERS
-            // if (this.applyPixel) {
-            //   this.applyPixel(i, j, iteration/iteration_number);
-            // }
+            mask[index] = iteration/iteration_number
             break;
+          
           } else if (boundaryCheck <= boundary && iteration == iteration_number) {
+            mask[index]=1
             //PAINTING INSIDE OF MANDELBROT SET
             if (this.applyPixel) {
-              this.applyPixel(i, j, 1,x_pixel,y_pixel);
+              this.applyPixel(i, j, 1,x_pixel_value,y_pixel_value);
             }
           }
         }
@@ -57,8 +70,7 @@ export default class MandelbrotCalculation {
     }
     
     const endTime = performance.now()
-    console.log(`Mandelbrot calculated in ${Math.round((endTime-startTime)/1000 * 100) / 100} seconds`);
+    console.log(`Mandelbrot calculated in ${Math.round((endTime - startTime) / 1000 * 100) / 100} seconds`);
+    return mask;
   }
 }
-
-// TODO: use Web Workers to optimize calculation
